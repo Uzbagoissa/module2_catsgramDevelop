19a5a94400113a6aba386b54a7562f118e170200
Stas <stas.vk88@yandex.ru>
2022-09-21 23:10:26 +0200

    8

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,17 +1,20 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@Slf4j
 public class SimpleController {
-    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
-
-    @GetMapping("/home")
-    public String homePage() {
-        log.info("Получен запрос.");
+    @RequestMapping("/home")
+    @ResponseBody
+    public String homePage(HttpServletRequest request) {
+        log.info("Получен запрос к эндпоинту: '{} {}', Строка параметров запроса: '{}'",
+                request.getMethod(), request.getRequestURI(), request.getQueryString());
         return "Котограм";
     }
 }
