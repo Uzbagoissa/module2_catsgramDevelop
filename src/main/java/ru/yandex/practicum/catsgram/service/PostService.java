a3c4baa9ea471691543788a26a6caffd58196f1d
Stas <stas.vk88@yandex.ru>
2022-10-01 22:23:27 +0200

    12

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.catsgram.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.yandex.practicum.catsgram.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -18,4 +20,23 @@ public class PostService {
         posts.add(post);
         return post;
     }
+
+    //Вариант метода способом с обходом коллекции через поток
+    public Optional<Post> findById(int postId) {
+        return posts.stream()
+                .filter(x -> x.getId() == postId)
+                .findFirst();
+    }
+
+    //Вариант метода обычным способом с обходом коллекции через цикл
+    /*public Optional<Post> findById(int postId) {
+        Post x = null;
+        for (int i = 0; i < posts.size(); i++) {
+            if (posts.get(i).getId() == postId){
+                x = posts.get(i);
+                break;
+            }
+        }
+        return Optional.ofNullable(x);
+    }*/
 }
